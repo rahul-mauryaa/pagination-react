74c847290222b318ae5ca070897f447657184e8a
rahulmaurya433 <rahulmaurya433456@gmail.com>
2023-05-23 11:43:43 +0530

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [productList, setProductList] = useState([]);
+  const [page,setPage] = useState(1);
+  console.log(productList, "productList");
+  const fetch_product = async () => {
+    const get_product = await fetch("https://dummyjson.com/products?limit=100");
+    const data = await get_product.json();
+    if (data && data.products) {
+
+      setProductList(data.products);
+    }
+  };
+
+
+  const selectedPage = (selected) =>{
+    if(selected >=1 && selected <= productList.length/10)
+    setPage(selected);
+  }
+
+  useEffect(() => {
+    fetch_product();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {productList.length > 0 && (
+        <div className="products">
+          {productList.slice(page * 10 - 10,page * 10).map((prod) => {
+            return (
+              <span className="products__single">
+                <img src={prod.thumbnail} alt={prod.title} />
+                <span>{prod.title}</span>
+              </span>
+            );
+          })}
+        </div>
+      )}  
+
+      {productList.length > 0 && <div className="pagination">
+      <span onClick={() => selectedPage(page - 1)} className={page > 1 ? "" : "pagination__disable"}>◀</span>
+            {[...Array(productList.length / 10)].map((_,i)=>{
+                return <span className={(page === i+1)?"pageselected":''} onClick={()=>selectedPage(i+1)}>{i+1}</span>
+            })}
+           <span onClick={()=>selectedPage(page + 1)} className={page < productList.length / 10 ? "" : "pagination__disable"}>▶</span>
+        </div>}
     </div>
   );
 }
